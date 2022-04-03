f49c2e355d36168e2c8be07943c43041e15d92bd
tina1998612 <tina26919742@gmail.com>
2022-04-03 18:03:44 +0000

    split into chunks

--- src/main.rs
+++ src/main.rs
@@ -26,29 +26,32 @@ fn main() {
     let mut wtr = csv::Writer::from_path("done.csv").unwrap();
     wtr.write_record(&["address", "amount", "tx_hash"]).unwrap();
 
-    for record in records.into_iter() {
+    let key_pair:Keypair = Keypair::from_base58_string(&env::var("KEY").unwrap());
+    let wallet_publickey = key_pair.pubkey();
+    let fee_payer = Some(&wallet_publickey);
+    let signers: Vec<&Keypair> = vec![&key_pair];
+    // change RPC endpoint here
+    let rpc_url: String = env::var("NETWORK_RPC").unwrap();
+    let commitment = CommitmentConfig::confirmed();
+    let rpc_client = RpcClient::new_with_commitment(&rpc_url, commitment);
+    let recent = rpc_client
+    .get_latest_blockhash()
+    .expect("failed to get recent blockhash");
+    let token_mint_pub = util::get_pub(&env::var("TOKEN_MINT_ADDR").unwrap());
+    let ata_sender = get_associated_token_address(&wallet_publickey,&token_mint_pub);
+
+
+    for chunk in records.chunks(10) {
+        let mut ins: Vec<Instruction> = vec![];
+
+    for record in chunk {
         println!("{:?}", record);
         // record.address
         // record.amount
 
-        let key_pair:Keypair = Keypair::from_base58_string(&env::var("KEY").unwrap());
-        let mut ins: Vec<Instruction> = vec![];
-        let wallet_publickey = key_pair.pubkey();
-        let fee_payer = Some(&wallet_publickey);
-        let signers: Vec<&Keypair> = vec![&key_pair];
-        // change RPC endpoint here
-        let rpc_url: String = env::var("NETWORK_RPC").unwrap();
-        let commitment = CommitmentConfig::confirmed();
-        let rpc_client = RpcClient::new_with_commitment(&rpc_url, commitment);
-        let recent = rpc_client
-        .get_latest_blockhash()
-        .expect("failed to get recent blockhash");
-
         let receiver_pub = util::get_pub(&record.address);
-        let token_mint_pub = util::get_pub(&env::var("TOKEN_MINT_ADDR").unwrap());
 
         let ata = get_associated_token_address(&receiver_pub,&token_mint_pub);
-        let ata_sender = get_associated_token_address(&wallet_publickey,&token_mint_pub);
         let create_ata_ins = create_associated_token_account(&wallet_publickey , &receiver_pub, &token_mint_pub);
         // creat if associated token account does not exist
         if rpc_client.get_account_with_commitment(&ata, CommitmentConfig::processed()).unwrap().value.is_none() {
@@ -63,7 +66,8 @@ fn main() {
             spl_token::ui_amount_to_amount(record.amount.parse::<f64>().unwrap(), env::var("TOKEN_DECIMAL").unwrap().parse::<u8>().unwrap()), // convert to raw amount according to the token decimal
         ).unwrap());
 
-        let mut tx = Transaction::new_with_payer(&ins, fee_payer);
+    }
+     let mut tx = Transaction::new_with_payer(&ins, fee_payer);
         tx.sign(&signers, recent);
         // let messagee = encode(tx.message_data());
 
@@ -79,10 +83,11 @@ fn main() {
         let tx_hash = send.unwrap();
 
         println!("Tx hash: {:?}", &tx_hash);
-        wtr.serialize((record.address, record.amount, tx_hash.to_string())).unwrap();
-
+        for rec in chunk {
+            wtr.serialize((&rec.address, &rec.amount, tx_hash.to_string())).unwrap();
+        }
+        wtr.flush().unwrap();
     }
-    wtr.flush().unwrap();
 
     // println!("Hello, world!");
 }
